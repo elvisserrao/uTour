c9189b8595062ba5fd6db7ccd5d7f5094abc2633
Elvis Serrão <elvis.serrao1@gmail.com>
2020-01-12 11:32:31 -0300

    Seed admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "*** Seeding Administrator"
+admin = User
+  .where(email: "elvis.serrao1@gmail.com")
+  .first_or_create(
+    first_name: 'Elvis',
+    last_name: 'Serrão',
+    password: 'password',
+    password_confirmation: 'password',
+    admin: true
+  )
