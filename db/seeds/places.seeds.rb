d4ac79db1420407313b41151fd75a652877b657a
Elvis Serrão <elvis.serrao1@gmail.com>
2020-01-15 23:38:58 -0300

    Add seeds

--- db/seeds/places.seeds.rb
+++ db/seeds/places.seeds.rb
@@ -1,12 +1,13 @@
 p '*** Seeding Places'
-names = ['Coco Bambu', 'Sal e Brasa', 'PastelBurg', 'Brothers Burguer']
-types = ['Churrascaria', 'Pizzaria', 'Restaurante']
-names.each do |name|
+names = ['Sal e Brasa', 'PastelBurg', 'Coco Bambu', 'Brothers Burguer', 'McDonalds', 'Rei do Pirão']
+types = ['Churrascaria', 'Pizzaria', 'Restaurante', 'Hamburgueria', 'Fast Food', 'Bar/Petiscaria']
+type = types.cycle.each
+names.each do |name, |
   place = Place
           .where(name: name)
           .first_or_create(
-            establishment: 'Restaurante',
+            establishment: type.next,
             description: 'Um bom lugar para comer e resenhar com os amigos',
           )
-end
-p "Created places: #{names.to_sentence}"
+          p "Created place: #{place.name}"
+    end
