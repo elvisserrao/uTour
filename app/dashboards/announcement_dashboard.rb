d08e1bed002993d037250945c88fb890c83302d7
Elvis Serrão <elvis.serrao1@gmail.com>
2020-01-19 23:19:43 -0300

    Add some translation

--- app/dashboards/announcement_dashboard.rb
+++ app/dashboards/announcement_dashboard.rb
@@ -65,8 +65,8 @@ class AnnouncementDashboard < Administrate::BaseDashboard
 
   # Overwrite this method to customize how announcements are displayed
   # across all pages of the admin dashboard.
-  #
-  # def display_resource(announcement)
-  #   "Announcement ##{announcement.id}"
-  # end
+
+  def display_resource(announcement)
+    "#{announcement.name}"
+  end
 end
